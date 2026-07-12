2954014965b569a643cf9e800133d08b9204affc
adixid <94796982+adixid@users.noreply.github.com>
2026-07-12 15:36:25 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -1,11 +1,20 @@
 const myDice = ["one", "two", "three", "four", "five", "six"];
 
-for (let i = 0; i < myDice.length; i++) {
+
+for (let i = 0; i < 20; i++) {
+
+  const randomNumber = Math.floor(Math.random() * 6);
+
   setTimeout(() => {
     myDice.forEach(face =>
       document.querySelector("." + face).style.display = "none"
     );
 
-    document.querySelector("." + myDice[i]).style.display = "flex";
-  }, i * 1000);
+    document.querySelector("." + myDice[randomNumber]).style.display = "flex";
+  }, i * 100);
 }
+
+
+    
+    
+  
