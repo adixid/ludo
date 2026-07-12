58b2ad69344833f6f56d5e441a0d625fddbc5a26
adixid <94796982+adixid@users.noreply.github.com>
2026-07-12 15:39:29 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -1,18 +1,20 @@
 const myDice = ["one", "two", "three", "four", "five", "six"];
 
-
-for (let i = 0; i < 20; i++) {
+function playDice () {
+  for (let i = 0; i < 10; i++) {
 
   const randomNumber = Math.floor(Math.random() * 6);
 
-  setTimeout(() => {
-    myDice.forEach(face =>
-      document.querySelector("." + face).style.display = "none"
-    );
+    setTimeout(() => {
+      myDice.forEach(face =>
+        document.querySelector("." + face).style.display = "none"
+      );
 
-    document.querySelector("." + myDice[randomNumber]).style.display = "flex";
-  }, i * 100);
+      document.querySelector("." + myDice[randomNumber]).style.display = "flex";
+    }, i * 100);
+  }
 }
+
 
 
     
--- info-panel.js
+++ info-panel.js
@@ -3,7 +3,7 @@ infoPanel.innerHTML = `
   <div class="info"></div>
   <div class="dice">
     <div class="roll-dice-title">ROLL DICE</div>
-    <div class="play-dice">
+    <div class="play-dice" onclick="playDice()">
   
       <div class="one">
         <div class="black-circle"></div>
