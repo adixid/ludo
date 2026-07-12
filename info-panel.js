3f56bd6f44cacbfc2e9c9b8bbc69d801692e7bb7
adixid <94796982+adixid@users.noreply.github.com>
2026-07-12 14:56:10 +0100

    Add files via upload

--- info-panel.js
+++ info-panel.js
@@ -9,6 +9,57 @@ infoPanel.innerHTML = `
         <div class="black-circle"></div>
       </div>
 
+      <div class="two">
+        <div class="black-circle" id="two-top"></div>
+        <div class="black-circle" id="two-bottom"></div>
+      </div>
+
+      <div class="three">
+        <div class="black-circle" id="three-top"></div>
+        <div class="black-circle" id="three-middle"></div>
+        <div class="black-circle" id="three-bottom"></div>
+      </div>
+
+      <div class="four">
+        <div class="four-row">
+          <div class="black-circle"></div>
+          <div class="black-circle"></div>
+        </div>
+        <div class="four-row">
+          <div class="black-circle"></div>
+          <div class="black-circle"></div>
+        </div>
+      </div>
+
+      <div class="five">
+        <div class="five-row">
+          <div class="black-circle"></div>
+          <div class="black-circle"></div>
+        </div>
+        <div class="five-row">
+          <div class="black-circle"></div>
+        </div>
+        <div class="five-row">
+          <div class="black-circle"></div>
+          <div class="black-circle"></div>
+        </div>
+      </div>
+
+      <div class="six">
+        <div class="six-row">
+          <div class="black-circle"></div>
+          <div class="black-circle"></div>
+        </div>
+         <div class="six-row">
+          <div class="black-circle"></div>
+          <div class="black-circle"></div>
+        </div>
+         <div class="six-row">
+          <div class="black-circle"></div>
+          <div class="black-circle"></div>
+        </div>
+      </div>
+
     </div>
   </div>
 
