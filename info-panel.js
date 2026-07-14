6fba605566c6a09f2d7dc376a39bdf5a6a3b39eb
adixid <94796982+adixid@users.noreply.github.com>
2026-07-14 07:45:26 +0100

    Add files via upload

--- info-panel.js
+++ info-panel.js
@@ -1,9 +1,9 @@
 const infoPanel = document.getElementById("info-panel");
 infoPanel.innerHTML = `
   <div class="info"></div>
-  <div class="dice">
+  <div class="dice" onclick="playDice()">
     <div class="roll-dice-title">ROLL DICE</div>
-    <div class="play-dice" onclick="playDice()">
+    <div class="play-dice">
   
       <div class="one">
         <div class="black-circle"></div>
